a42412636629f8d4c1701b86b64f91828e3e9e20
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-07-02 18:04:59 +0530

    code logic implemented

--- towerofhanoi.java
+++ towerofhanoi.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class towerofhanoi {
-    
+    public static void towerOfHanoi(int n, String Source, String Helper, String Destination){
+        if(n==1){
+            System.out.println("Transfer disk "+ n + " from "+Source+" to "+Destination);
+            return;
+        }
+        
+           towerOfHanoi(n-1, Source, Destination, Helper);
+           System.out.println("Transfer disk "+ n + " from "+Source+" to "+Destination);
+           return;
+}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number of disk: ");
+        int n = sc.nextInt();
+        towerOfHanoi(n,"S", "H", "D");
+    }
 }
